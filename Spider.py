a18d141a54b047eaf891dbf14e03a7086e22b1b9
Saumitra Bose <imtoobose@gmail.com>
2016-03-16 01:16:08 +0530

    Regex for checking album and retrieving links added

--- Spider.py
+++ Spider.py
@@ -6,10 +6,20 @@ import re
 r= praw.Reddit(user_agent= "Wallpaper fetch by Bose")
 walls= r.get_subreddit('wallpapers').get_top()
 urls= [str(x.url) for x in walls]
-chk_a= re.search('')
+
 for j in urls:
 	#we want to check if j is album
 	#or not
-	
-	link= requests.get(j)
-	soup= BeautifulSoup(open(link.content))
+
+	found= re.search('(imgur.com/a/)+', j)
+	if found:
+		vals= []
+		link= requests.get(j)
+		soup= BeautifulSoup(link.content)
+		img_links= [str(x) for x in soup.select(".zoom img")]
+		for k in img_links:
+			imgs= re.search('src="//(.+?)"/>', k)
+			if imgs:
+				vals.append(imgs.group(1))	
+		print j			
+		print vals
