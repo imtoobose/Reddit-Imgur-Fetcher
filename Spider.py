76d85f2d2c8a48dc70290280d6a8312a853c2353
Saumitra Bose <imtoobose@gmail.com>
2016-03-16 00:59:31 +0530

    Changes made again

--- Spider.py
+++ Spider.py
@@ -1,7 +1,15 @@
 import praw
-from bs4 import beautifulsoup4
+import requests
+from bs4 import BeautifulSoup
+import re
 
-r= praw.Reddit(user_agent= "Wallpaper fetch by Bose"
+r= praw.Reddit(user_agent= "Wallpaper fetch by Bose")
 walls= r.get_subreddit('wallpapers').get_top()
 urls= [str(x.url) for x in walls]
-
+chk_a= re.search('')
+for j in urls:
+	#we want to check if j is album
+	#or not
+	
+	link= requests.get(j)
+	soup= BeautifulSoup(open(link.content))
