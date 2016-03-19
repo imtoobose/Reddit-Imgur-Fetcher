1347ccd8af05d7d681fd3f0b3a9074f84277d9c1
Saumitra Bose <imtoobose@gmail.com>
2016-03-19 21:39:36 +0530

    Interface added

--- Image_Fetcher.py
+++ Image_Fetcher.py
@@ -18,15 +18,56 @@ def chk_not_reddit(pp):
 		if i=="www.reddit.com":
 			return false
 	
-
 print "Reddit Imgur Image Fetch Running v.1.0.1"
+print "Enter subreddit name:\n"
+subreddit_n= stdin.readline().strip()
+flag=0
 
-folder    = r"C:\Users\BOSE\Documents\2016\Code\image"
-r         = praw.Reddit(user_agent= "Wallpaper fetch by Bose")
-walls     = r.get_subreddit('wallpapers').get_top_from_week(limit=10)	#fetch top 10 images
-urls      = [str(x.url) for x in walls]									#fetch urls and store as strings
-chk_imgur = re.compile('(imgur\.com)+?')								#check if imgur page
-counter   = 0
+while flag==0:
+	print "Enter choice:\n1.Top\n2.Hot\n"
+	try:
+		choice    = int(stdin.readline())
+	except TypeError:
+		print "Invalid input"
+		continue
+
+	if choice== 1:
+		print "1.Top all time\n2. Top Weekly\n3. Top Daily\n4.Top Hourly\n"
+		try:
+			answer= int(stdin.readline())
+		except TypeError:
+			print "Invalid input"
+			continue
+
+		if answer   == 1 :   
+			red, flag = get_top_from_all,   1
+		elif answer == 2 :
+			red, flag = get_top_from_week,  1 
+		elif answer == 3 :   
+			red, flag = get_top_from_day,   1
+		elif answer == 4 : 
+			red, flag = get_top_from_hour,  1
+		else:
+			print "Invalid input"
+
+	elif choice==2:
+		red, flag = get_hot, 1
+
+	else:
+		continue		
+
+print "Select number of links to be fetched (max 100, default 5)\n"
+try:
+	no_of_links = int(stdin.readline())
+except TypeError:
+	no_of_links = 5
+
+folder      = r"C:\Users\BOSE\Documents\2016\Code\image"
+r           = praw.Reddit(user_agent= "Wallpaper fetch by Bose")
+walls       = r.get_subreddit('wallpapers').red(limit= no_of_links)		#fetch top 10 images
+urls        = [str(x.url) for x in walls]								#fetch urls and store as strings
+chk_imgur   = re.compile('(imgur\.com)+?')								#check if imgur page
+counter     = 0
 
 
 print "The retrieved links are:\n"
